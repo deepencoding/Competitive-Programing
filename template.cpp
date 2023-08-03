1238c33dc3a09e11f6a7d395af10cf5e154ce16b
Deepanshu Gautam <deepanshugautam5111004@gmail.com>
2023-08-04 00:36:38 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,24 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void func() {
+#define int long long int
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define vb vector<bool>
+#define pi pair<int, int>
+#define si set<int>
+#define rep(var, l, r) for (int var = l, var < r; var++)
+
+
+void solve() {
     
-    
-    return;
 }
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    
-    
-    
-    int T;
-    cin >> T;
-    
-    while (T--) {
-        int temp;
-        func();
-    }
+int32_t main() {
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+    int tc = 1;
+    cin >> tc;
+    while (tc--)
+        solve();
     return 0;
 }
