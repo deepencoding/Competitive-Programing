067ffef814805447a6132e5af583f40cea74f088
Deepanshu Gautam <deepanshugautam5111004@gmail.com>
2023-08-06 02:21:19 +0530

    Create A - Chord.cpp

---
+++ AtCoder/AtCoder Beginner Contest 313/A - Chord.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+bool chord(string s)
+{
+	vector<string> word = {"ACE","BDF","CEG","DFA","EGB","FAC","GBD"};
+	for (unsigned i = 0; i < word.size(); i++)
+	{
+		if (s == word[i]) { return true; }
+	}
+	return false;
+	
+}
+		
+ 
+int main()
+{
+	string s;
+	cin >> s;
+	if (chord(s)) { cout << "Yes"; }
+	else { cout << "No"; }
+	return 0;
+}
