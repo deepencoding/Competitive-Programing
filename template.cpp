3e66fb1937e32c3fdce05db0813c8b8310796cde
Deepanshu Gautam <deepanshugautam5111004@gmail.com>
2023-08-16 20:16:04 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -3,20 +3,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define pb push_back
-#define float long double
-#define all(v) v.begin(), v.end()
-#define sz(a) (int)a.size()
-#define M 1e9+7
-#define endl "\n"
-#define int long long int
-#define vi vector<int>
-#define vvi vector<vector<int>>
-#define vb vector<bool>
-#define vs vector<string>
-#define pi pair<int, int>
-#define si set<int>
-#define rep(var, l, r) for (int var = l; var < r; var++)
+#define   pb                   push_back
+#define   float                long double
+#define   all(v)               v.begin(), v.end()
+#define   sz(a)                (int)a.size()
+#define   M                    1e9+7
+#define   inf                  1e18
+#define   endl                 "\n"
+#define   int                  long long int
+#define   vi                   vector<int>
+#define   mii                  map<int,int>
+#define   mmii                 multimap<int,int>
+#define   umii                 unordered_map<int,int>
+#define   mci                  map<char,int>
+#define   mmci                 multimap<char,int>
+#define   umci                 unordered_map<char,int>
+#define   vvi                  vector<vector<int>>
+#define   vb                   vector<bool>
+#define   vs                   vector<string>
+#define   pii                  pair<int, int>
+#define   si                   set<int>
+#define   usi                  unordered_set<int>
+#define   msi                  multiset<int>
+#define   sc                   set<char>
+#define   msc                  multiset<char>
+#define   usc                  unordered_set<char>
+#define   pqmax                priority_queue<int> // max heap
+#define   pqmin                priority_queue<int,vi,greater<int>> // min heap
+#define   rep(var, l, r)       for (int var = l; var < r; var++)
+#define   revrep(var, r, l)    for (int var = r; var >= l; var--)
+#define   setbits(x)           __builtin_popcountll(x)
+#define   zerobits(x)          __builtin_ctzll(x)
+#define   ldzrobits(x)         __builtin_clzll(x)
+#define   trzrobits(x)         __builtin_ctzll(x)
+#define   parity(x)            __builtin_parityll(x) // Returns 1 if the number has odd parity else it returns 0
 
 
 void solve() {
