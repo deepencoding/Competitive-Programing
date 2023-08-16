482fe2143218a7690c6a91ea2792ac3f968ebd60
Deepanshu Gautam <deepanshugautam5111004@gmail.com>
2023-08-17 00:18:02 +0530

    Create A - Buttons.cpp

---
+++ Codeforces/Codeforces Round 893 (Div. 2)/A - Buttons.cpp
@@ -0,0 +1,53 @@
+// deepencoding
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define float long double
+#define all(v) v.begin(), v.end()
+#define sz(a) (int)a.size()
+#define M 1e9+7
+#define endl "\n"
+#define int long long int
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define vb vector<bool>
+#define vs vector<string>
+#define pi pair<int, int>
+#define si set<int>
+#define rep(var, l, r) for (int var = l; var < r; var++)
+
+
+void solve() {
+    int a, b, c;
+    cin >> a >> b >> c;
+    
+    if (c%2 == 0){
+		if (a <= b){
+			cout << "Second" << endl;
+			return;
+		} else {
+			cout << "First" << endl;
+			return;
+		}
+	} else {
+		if (a >= b){
+			cout << "First" << endl;
+			return;
+		} else {
+			cout << "Second" << endl;
+			return;
+		}
+	}
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+    int tc = 1;
+    cin >> tc;
+    while (tc--)
+        solve();
+    return 0;
+}
