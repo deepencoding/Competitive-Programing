05184876d2f3af8f20331efafc645eeaaa107aa1
Deepanshu Gautam <deepanshugautam5111004@gmail.com>
2023-09-18 22:32:27 +0530

    Create A - MEXanized Array.cpp

---
+++ Codeforces/CodeTON Round 6 (Div. 1 + Div. 2, Rated, Prizes!)/A - MEXanized Array.cpp
@@ -0,0 +1,81 @@
+// deepencoding
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Macros
+
+// Shortners
+#define   M                    1e9+7
+#define   endl                 "\n"
+#define   int                  long long int
+#define   pb                   push_back
+#define   float                long double
+#define   all(v)               v.begin(), v.end()
+#define   sz(a)                (int)(a).size()
+#define   inf                  1e18
+
+// Containers
+#define   vi                   vector<int>
+#define   mii                  map<int,int>
+#define   mmii                 multimap<int,int>
+#define   umii                 unordered_map<int,int>
+#define   mci                  map<char,int>
+#define   mmci                 multimap<char,int>
+#define   umci                 unordered_map<char,int>
+#define   vvi                  vector<vector<int>>
+#define   vb                   vector<bool>
+#define   vs                   vector<string>
+#define   pii                  pair<int, int>
+#define   si                   set<int>
+#define   usi                  unordered_set<int>
+#define   msi                  multiset<int>
+#define   sc                   set<char>
+#define   msc                  multiset<char>
+#define   usc                  unordered_set<char>
+#define   pqmax                priority_queue<int> // max heap
+#define   pqmin                priority_queue<int,vi,greater<int>> // min heap
+
+// Functionality
+#define   rep(var, l, r)       for (int var = l; var < r; var++) // Not inclusive on the right side
+#define   revrep(var, r, l)    for (int var = r; var >= l; var--) // Inclusive
+#define   setbits(x)           __builtin_popcountll(x)
+#define   zerobits(x)          __builtin_ctzll(x)
+#define   ldzrobits(x)         __builtin_clzll(x)
+#define   trzrobits(x)         __builtin_ctzll(x)
+#define   parity(x)            __builtin_parityll(x) // Returns 1 if the number has odd parity else it returns 0
+#define   watch(x)             cerr << "\n" << (#x) << " is " << (x) << endl // print a var to stderr stream (invis to ONLINE JUDGE)
+
+// Solution
+void solve() { // 5 3 3
+    int n, k, x;
+    cin >> n >> k >> x;
+    int sum = 0;
+    if ((k > n) || ((k-x) > 1)){
+		cout << -1 << '\n';
+		return;
+	}
+    rep (i, 0, n){
+		if(i<k){
+			sum+=i;
+		} else {
+			if (k != x){
+				sum+=x;
+			} else {
+				sum+=(x-1);
+			}
+		}
+	}
+	cout << sum << '\n';    
+}
+
+// Driver Code
+int32_t main() {
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+    int tc = 1;
+    cin >> tc;
+    while (tc--)
+        solve();
+    return 0;
+}
